3a9e12af70ac4ab5af39f1f298e6116f495c998c
ArRaudhatulPutriMuhida <putri.muhida@gmail.com>
2025-10-05 23:41:39 +0700

    formatter.py

--- SINF2013_PrakKomNum_C_UTS_Klp5_NewtonRaphson/core/formatter.py
+++ SINF2013_PrakKomNum_C_UTS_Klp5_NewtonRaphson/core/formatter.py
@@ -1,1 +1,7 @@
+import re
 
+def format_persamaan(teks):
+    teks = teks.replace("^", "**").replace("=0", "").strip()
+    teks = re.sub(r'(\d)([a-zA-Z])', r'\1*\2', teks)
+    teks = re.sub(r'([a-zA-Z])(\d)', r'\1*\2', teks)
+    return teks
