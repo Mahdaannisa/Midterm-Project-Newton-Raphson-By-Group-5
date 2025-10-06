bef8d7ca8df3882e68e15b206cb048d076904190
ArRaudhatulPutriMuhida <putri.muhida@gmail.com>
2025-10-06 11:02:42 +0700

    formatter.py

--- SINF2013_PrakKomNum_C_UTS_Klp5_NewtonRaphson/core/formatter.py
+++ SINF2013_PrakKomNum_C_UTS_Klp5_NewtonRaphson/core/formatter.py
@@ -5,3 +5,9 @@ def format_persamaan(teks):
     teks = re.sub(r'(\d)([a-zA-Z])', r'\1*\2', teks)
     teks = re.sub(r'([a-zA-Z])(\d)', r'\1*\2', teks)
     return teks
+
+def fmt_excel(v):
+    try:
+        return f"{float(v):.9f}".rstrip('0').rstrip('.') if '.' in f"{float(v):.9f}" else f"{float(v):.9f}"
+    except:
+        return str(v)
