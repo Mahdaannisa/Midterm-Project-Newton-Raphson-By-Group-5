954df852686bb0f694d2caec89039de1ca43ef45
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-10-06 19:44:38 +0700

    Update newton.py

--- SINF2013_PrakKomNum_C_UTS_Klp5_NewtonRaphson/core/newton.py
+++ SINF2013_PrakKomNum_C_UTS_Klp5_NewtonRaphson/core/newton.py
@@ -1,1 +1,9 @@
+from sympy import symbols, sympify, diff, lambdify
 
+def newton_raphson(expr_str, x0, n_iter=10, tol=1e-6):
+  x = symbols('x')
+  f_sym = sympify(expr_str)
+  df_sym = diff(f_sym, x)
+  f = lambdify(x, f_sym, 'numpy')
+  df = lambdify(x, df_sym, 'numpy')
+  
