c3b38e806ad04cfa1b60fc3ea05b096095310cdc
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-10-07 23:50:49 +0700

    Update formatter.py

--- SINF2013_PrakKomNum_C_UTS_Klp5_NewtonRaphson/core/formatter.py
+++ SINF2013_PrakKomNum_C_UTS_Klp5_NewtonRaphson/core/formatter.py
@@ -5,9 +5,3 @@ def format_persamaan(teks):
     teks = re.sub(r'(\d)([a-zA-Z])', r'\1*\2', teks)
     teks = re.sub(r'([a-zA-Z])(\d)', r'\1*\2', teks)
     return teks
-
-def fmt_excel(v):
-    try:
-        return f"{float(v):.9f}".rstrip('0').rstrip('.') if '.' in f"{float(v):.9f}" else f"{float(v):.9f}"
-    except:
-        return str(v)
